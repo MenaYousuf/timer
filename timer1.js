d3625fdbb54997268ecc58a83a9bf93451143f48
MenaYousuf <menayousuf99@gmail.com>
2021-11-03 02:40:26 +0000

    Timer countdown

---
+++ timer1.js
@@ -0,0 +1,23 @@
+// const diffTimer = function(time) {
+//   let times = []
+//   for (let i = 0; i < time.length; i++) {
+//     if (time[i] === 3 || 5 || 9 || 10 || 15)
+//       return ("beep");
+//   };
+//   }
+ 
+
+
+const diffTimes = function(num){
+let delay = 1000
+
+for (let i = 0; i < num.length; i++){
+  if (num = num.length[i]);{
+    return num[i];
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, delay);
+}
+}console.log(diffTimes())
+}
+
